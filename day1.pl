aa06047c01a4cd90202f0a17a51cdb1bf7cce290
Ani_03 <140332086+Anidipta@users.noreply.github.com>
2025-02-20 01:36:33 +0530

    Add files via upload

---
+++ day1.pl
@@ -0,0 +1,59 @@
+parent(nilesh, ravi).
+parent(nita, kiran).
+parent(nilesh, kiran).
+parent(nita, ravi).
+parent(rakesh, nita).
+parent(pramila, nita).
+parent(mira, amitav).
+parent(rakesh, mira).
+parent(pramila, mira).
+parent(amit, ramila).
+parent(subhra, ramila).
+parent(rakesh, amit).
+parent(pramila, amit).
+parent(abhishek, kavita).
+parent(kamla, kavita).
+parent(rakesh, abhishek).
+parent(pramila, abhishek).
+
+male(rakesh).
+male(nilesh).
+male(amit).
+male(abhishek).
+male(ravi).
+male(amitav).
+
+female(pramila).
+female(nita).
+female(mira).
+female(subhra).
+female(kamla).
+female(kiran).
+female(ramila).
+female(kavita).
+
+marriedto(rakesh, pramila).
+marriedto(nilesh, nita).
+marriedto(amit, subhra).
+marriedto(abhishek, kamla).
+
+mother(X, Y) :- parent(X, Y), female(X).
+father(X, Y) :- parent(X, Y), male(X).
+
+son(X, Y) :- parent(Y, X), male(X).
+daughter(X, Y) :- parent(Y, X), female(X).
+
+siblings(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+
+uncle(X, Y) :- parent(Z, Y), siblings(X, Z), male(X).
+aunt(X, Y) :- parent(Z, Y), siblings(X, Z), female(X).
+
+mgm(X, Y) :- mother(Z, Y), mother(X, Z).
+mgf(X, Y) :- mother(Z, Y), father(X, Z).
+pgf(X, Y) :- father(Z, Y), father(X, Z).
+pgm(X, Y) :- father(Z, Y), mother(X, Z).
+
+wife(X, Y) :- marriedto(Y, X), female(X).
+
+sil(X, Y) :- parent(Y, Z), marriedto(X, Z), male(X).
+dil(X, Y) :- parent(Y, Z), marriedto(Z, X), female(X).
